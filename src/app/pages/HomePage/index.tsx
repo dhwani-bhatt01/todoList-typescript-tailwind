177d810bedfbad5e2e8e0777e57c409303e20ff9
Indranil Tiwary <indraniltiwary@hotmail.com>
2021-05-16 15:35:06 +0530

    Demo Tailwind in Homepage

--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -8,7 +8,18 @@ export function HomePage() {
         <title>Home Page</title>
         <meta name="description" content="A Boilerplate application homepage" />
       </Helmet>
-      <span>HomePage container</span>
+      <div className="container">
+        <span className="font-bold text-red-400">HomePage container</span>
+        <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+          <div className="flex-shrink-0">
+            <img className="h-12 w-12" src="/logo192.png" alt="ChitChat Logo" />
+          </div>
+          <div>
+            <div className="text-xl font-medium text-black">ChitChat</div>
+            <p className="text-gray-500">You have a new message!</p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
